c0133fff1312ed03649e4c70bee5a224eced7c04
davidosili <147920967+davidosili@users.noreply.github.com>
2025-03-31 18:31:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,11 +29,7 @@ app.use(express.json());
 app.use(cors()); 
 
 // If you want to restrict CORS to your frontend only:
-app.use(cors({
-    origin: "http://127.0.0.1:5501",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
-}));
+
 
 app.use("/api", pollRoutes);
 
